029ec168a491b7ce12052c5605ede187e02ac64e
KeijiPlata <lordmiackykeijip@gmail.com>
2023-01-12 11:12:26 +0800

    Connect login form and student form

--- joysis_ams/Joysis_AMS/src/joysis_ams/ConnectionDB.java
+++ joysis_ams/Joysis_AMS/src/joysis_ams/ConnectionDB.java
@@ -6,6 +6,13 @@ import javax.swing.JOptionPane;
 public class ConnectionDB {
     // attributes
     static boolean validation;
+    static String uname;
+    static String fname;
+    static String mname;
+    static String lname;
+    static String sec;
+    static int id;
+    
     public void connection(String username, String password, String qry){
         try{
             String db = "jdbc:mysql://localhost:3306/joysis_ams";
@@ -18,12 +25,36 @@ public class ConnectionDB {
                 JOptionPane.showMessageDialog(null, "ADMIN DETECTED \n"
                                 + "ACCESS GRANTED!");
                 validation = true;
+                uname = username;
               
             } else {
                    JOptionPane.showMessageDialog(null, "Pakyu mali input \n"
                                 + "ACCESS NOT GRANTED!");
                  validation = false;
               }
+        }
+        catch(Exception a){
+            JOptionPane.showMessageDialog(null, a);
+            a.printStackTrace();
+        }
+    }
+    
+    public void getUsername(String username){
+        try{
+            String db = "jdbc:mysql://localhost:3306/joysis_ams";
+            Connection conn = DriverManager.getConnection(db, "root", null);
+            String qry = "SELECT * FROM student WHERE username = ?";
+            PreparedStatement ps = conn.prepareStatement(qry);
+            ps.setString(1, username);
+            ResultSet rslt = ps.executeQuery();
+            while(rslt.next()){
+                id = rslt.getInt("student_id");
+                fname = rslt.getString("firstname");
+                mname = rslt.getString("middlename");
+                lname = rslt.getString("lastname");
+                sec = rslt.getString("section");
+              
+            } 
         }
         catch(Exception a){
             JOptionPane.showMessageDialog(null, a);
